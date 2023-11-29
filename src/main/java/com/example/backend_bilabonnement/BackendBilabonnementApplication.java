1fcdf2800e56f04445f1c44256bbd41ecf176bbd
Alisashala <alis0216@tud.kea.dk>
2023-11-29 12:29:55 +0100

    test

---
+++ src/main/java/com/example/backend_bilabonnement/BackendBilabonnementApplication.java
@@ -0,0 +1,15 @@
+package com.example.backend_bilabonnement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBilabonnementApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBilabonnementApplication.class, args);
+		System.out.println("hej");
+	}
+
+}
